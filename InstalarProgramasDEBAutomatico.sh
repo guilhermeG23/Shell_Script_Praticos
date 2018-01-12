d53ba6a66a0cabf2cbd38e50aa3cfcaf725ccc6f
Bius <guilhermebrechot@gmail.com>
2018-01-12 08:41:19 -0200

    Install Automatico de programas Uteis Linux
    Pra facilitar em casos futuros

---
+++ InstalarProgramasDEBAutomatico.sh
@@ -0,0 +1,12 @@
+#Instalador de programas
+apt update
+apt upgrade -y
+apt install -y tmux
+apt install -y htop
+apt install -y vim
+apt install -y csh
+apt install -y zsh
+apt install -y git
+apt install -y curl
+apt install -y vlc
+curl -sS https://get.docker.com/ | sh
