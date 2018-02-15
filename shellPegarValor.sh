89b54a33a2cb73b9b180beaec61fa1e2f93fdf23
Bius <guilhermebrechot@gmail.com>
2018-02-15 14:59:22 -0200

    Alterando o shell da Temp

--- shellPegarValor.sh
+++ shellPegarValor.sh
@@ -1,5 +1,3 @@
 #Obter a temperatura do server
 #teste para obter a temperatura
-curl http://medidor/values.xml > teste.xml && echo Temperatura: $(grep -E "<Value\>" teste.xml | cut -c 10-13) $(date) >> SaidaTemperatura.txt && rm -rf teste.xml && cat SaidaTemperatura.txt 
-#Isso e para a aplicacao
-#cat SaidaTemperatura.txt | awk '{print $2}' > T.txt 
+curl http://192.168.112.97/values.xml > teste.xml && echo Temperatura: $(grep -E "<Value\>" teste.xml | cut -c 10-13) $(date) > SaidaTemperatura.txt && rm -rf teste.xml && cat SaidaTemperatura.txt >> History.txt && cat SaidaTemperatura.txt && cat SaidaTemperatura.txt | awk '{print $2}' > ProgramaTemperatura.txt 
